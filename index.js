a35f002be6a3c8b08632c3ecdf5f55f2d0efa6fb
Victoria <vmcdaniel459@gmail.com>
2024-06-28 16:25:36 -0400

    good job

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+console.log(dodger.style.left.replace('px',''));
+
+function moveDodgerLeft() {
+const leftNumbers = dodger.style.left.replace("px", "");
+const left = parseInt(leftNumbers, 10);
+
+if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+}
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+    }
+});
+console.log(dodger.style.left);
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+    }
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+    }
+});
+
+
+
+
+
+
+
+
+    
+    //const rightNumbers = dodger.style.right.replace('px','');
